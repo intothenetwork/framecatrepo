e133e34143ced89fbd14c32160c3c73cc26a3776
intothenetwork <flamelakey@gmail.com>
2024-09-03 14:58:04 +0800

    bigcat_Static

--- api/catindex.py
+++ api/catindex.py
@@ -1,11 +1,13 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'It"s Big Cat!'
+    # return ('It"s Big Cat!')
+    return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return 'About'
+
+if __name__ == "__main__":
+  app.run()
+  
+  
